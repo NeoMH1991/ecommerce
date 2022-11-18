78812e852287c5705339199ecb9a9a5b432a5a90
NeoMH1991 <neomenghwee1991@gmail.com>
2022-11-18 09:48:28 +0800

    added product page

---
+++ pages/products.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Products() {
+  return (
+    <div>
+      <div className="product1">
+        <h1>Cool Product 1</h1>
+        <h2>Very nice product 1</h2>
+        <h3>Price: $2.50</h3>
+      </div>
+      <h2>----------------------------</h2>
+      <div className="product2">
+        <h1>Cool Product 2</h1>
+        <h2>Very nice product 2</h2>
+        <h3>Price: $3.50</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Products;
